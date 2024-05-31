186e9d7a2dae75ffad03b5c9173e344c06e46d46
aligned64 <113694089+aligned64@users.noreply.github.com>
2024-05-31 13:52:00 +0300

    Added comments

--- src/main/java/HRInterface.java
+++ src/main/java/HRInterface.java
@@ -7,9 +7,16 @@ import java.util.Map;
  * HRInterface
  */
 public interface HRInterface {
+    /* Character = room type, Integer = available rooms left */
     Map<Character, Integer> listRooms();
+    /* Accepts room type, returns room cost */
     int getRoomCost(char c);
+    /* Books a room, if available. Accepts room type, number of rooms and the 
+       guest's name. Returns true if successful otherwise false. */
     Boolean bookRoom(char type, int number, String name);
+    /* Lists all guests */
     Vector<IGuest> listGuests();
+    /* Cancels a reservation, if made. Accepts room type, number of rooms and 
+       the guest's name. Returns true if successful otherwise false.  */
     Boolean cancelRoom(char type, int number, String name);
 }
